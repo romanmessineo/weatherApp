56ffda4ab522ba2cea1cc5fa5de96ae220e060df
roman.messieo <messineoroman@gmail.com>
2023-06-28 20:41:31 -0300

    git commit -m

---
+++ assets/js/api.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const api_key = "faa542c663f28dfec948592ea3827126";
+
+/** 
+*@param { String } URL Api url
+*@param {function} callback callback
+*/
+
+export const fetchData = function (URL, callback) {
+    fetch(`${URL}&appid=${api_key}`)
+        .then(res => res.json())
+        .then(data => callback(data));
+   
+}
+
+export const url = {
+    currentWeather(lat, lon) {
+        return `https://api.openweathermap.org/data/2.5/weather?${lat}&${lon}&units=metric`
+    },
+
+    forecast(lat, lon) {
+        return `https://api.openweathermap.org/data/2.5/forecast?${lat}&${lon}&units=metric`
+    },
+    airPollution(lat, lon) {
+        return `http://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}`
+    },
+    reverseGeo(lat, lon) {
+        return `http://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5`
+    },
+
+    /**
+    * @param {string} query Search query e.g.: "London", "New York"
+    */
+
+    geo(query) {
+        return `http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`
+    }
+
+
+
+}
