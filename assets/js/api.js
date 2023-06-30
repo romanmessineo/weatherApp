2b4d127a96f32d10abcee7a489778b1dabae40c8
roman.messieo <messineoroman@gmail.com>
2023-06-29 21:31:26 -0300

    Agrega funcionalidad de búsqueda de clima por ubicación

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,13 +1,14 @@
 'use strict';
 
-const api_key = "faa542c663f28dfec948592ea3827126";
+import { API_KEY } from "./config.js";
+
 
 /**
 *@param {String} URL Api url
 *@param {function} callback callback
 */
 export const fetchData = function (URL, callback) {
-    fetch(`${URL}&appid=${api_key}`)
+    fetch(`${URL}&appid=${API_KEY}`)
         .then(res => res.json())
         .then(data => callback(data));
 }
@@ -20,15 +21,15 @@ export const url = {
         return `https://api.openweathermap.org/data/2.5/forecast?${lat}&${lon}&units=metric`
     },
     airPollution(lat, lon) {
-        return `http://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}`
+        return `https://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}`
     },
     reverseGeo(lat, lon) {
-        return `http://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5`
+        return `https://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5`
     },
     /**
     * @param {string} query Search query e.g.: "London", "New York"
     */
     geo(query) {
-        return `http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`
+        return `https://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`
     }
 }
